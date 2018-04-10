e1d93c2632849a19c074bbf03864dd89fcc7c575
orbitalforcing <32709419+orbitalforcing@users.noreply.github.com>
2018-04-10 19:04:45 +0000

    Create 7-pwm.py

---
+++ 7-pwm.py
@@ -0,0 +1,97 @@
+# CamJam EduKit 3 - Robotics
+# Worksheet 7 – controlling motors with PWM
+
+import RPi.GPIO as GPIO # Import the GPIO Library
+import time # Import the Time library
+
+# Set the GPIO modes
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+# Set variables for the GPIO motor pins
+pinMotorAForwards = 10
+pinMotorABackwards = 9
+pinMotorBForwards = 8
+pinMotorBBackwards = 7
+
+# how many times to turn pin off each second
+Frequency = 20
+# how long pin stays on each cycle as a %
+DutyCycleA = 30
+DutyCycleB = 30
+# setting duty cycle to 0 will stop motors
+Stop = 0
+
+# Set the GPIO Pin mode to be Output
+GPIO.setup(pinMotorAForwards, GPIO.OUT)
+GPIO.setup(pinMotorABackwards, GPIO.OUT)
+GPIO.setup(pinMotorBForwards, GPIO.OUT)
+GPIO.setup(pinMotorBBackwards, GPIO.OUT)
+
+# set the GPIO to software PWM at at frequency Hz
+pwmMotorAForwards = GPIO.PWM(pinMotorAForwards, Frequency)
+pwmMotorABackwards = GPIO.PWM(pinMotorABackwards, Frequency)
+pwmMotorBForwards = GPIO.PWM(pinMotorBForwards, Frequency)
+pwmMotorBBackwards = GPIO.PWM(pinMotorBBackwards, Frequency)
+
+# start software PWM with duty cycle of 0 (not moving)
+pwmMotorAForwards.start(Stop)
+pwmMotorABackwards.start(Stop)
+pwmMotorBForwards.start(Stop)
+pwmMotorBBackwards.start(Stop)
+
+# Turn all motors off
+def StopMotors():
+    pwmMotorAForwards.ChangeDutyCycle(Stop)
+    pwmMotorABackwards.ChangeDutyCycle(Stop)
+    pwmMotorBForwards.ChangeDutyCycle(Stop)
+    pwmMotorBBackwards.ChangeDutyCycle(Stop)
+
+# Turn both motors forwards
+def Forwards():
+    pwmMotorAForwards.ChangeDutyCycle(DutyCycleA)
+    pwmMotorABackwards.ChangeDutyCycle(Stop)
+    pwmMotorBForwards.ChangeDutyCycle(DutyCycleB)
+    pwmMotorBBackwards.ChangeDutyCycle(Stop)
+
+# Turn both motors backwards
+def Backwards():
+    pwmMotorAForwards.ChangeDutyCycle(Stop)
+    pwmMotorABackwards.ChangeDutyCycle(DutyCycleA)
+    pwmMotorBForwards.ChangeDutyCycle(Stop)
+    pwmMotorBBackwards.ChangeDutyCycle(DutyCycleB)
+
+# turn left
+def Left():
+    pwmMotorAForwards.ChangeDutyCycle(Stop)
+    pwmMotorABackwards.ChangeDutyCycle(DutyCycleA)
+    pwmMotorBForwards.ChangeDutyCycle(DutyCycleB)
+    pwmMotorBBackwards.ChangeDutyCycle(Stop)
+
+# turn right
+def Right():
+    pwmMotorAForwards.ChangeDutyCycle(DutyCycleA)
+    pwmMotorABackwards.ChangeDutyCycle(Stop)
+    pwmMotorBForwards.ChangeDutyCycle(Stop)
+    pwmMotorBBackwards.ChangeDutyCycle(DutyCycleB)
+
+
+#Forwards()
+#time.sleep(5)
+
+#Left()
+#time.sleep(5)
+
+#Forwards()
+#time.sleep(1)
+
+#Right()
+#time.sleep(5)
+
+Backwards()
+time.sleep(5)
+
+
+StopMotors()
+
+GPIO.cleanup()
